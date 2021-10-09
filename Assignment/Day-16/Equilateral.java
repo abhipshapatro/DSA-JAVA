7daa77cc8c38c8679fa6d592a619e90f9a46d3d2
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2021-10-09 23:43:05 +0530

    updated

--- Assignment/Day-16/Equilateral.java
+++ Assignment/Day-16/Equilateral.java
@@ -5,8 +5,8 @@ public class Equilateral {
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        double area = Math.sqrt(3)/4 * a*a;
+        int side = s.nextInt();
+        double area = Math.sqrt(3)/4 * side*side;
         System.out.println("area of equilateral triangle is "+ area);
         s.close();
     }
