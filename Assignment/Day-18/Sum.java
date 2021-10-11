49fe3c462c99a4f2f2e4b251bcaa71dbcb96bb42
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2021-10-11 18:06:23 +0530

    Sum till input is 0

---
+++ Assignment/Day-18/Sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int sum =0;
+        while (true) {
+            
+            int n = s.nextInt();
+            if (n == 0) {
+                break;
+            }
+            sum += n;
+        }
+        System.out.println("sum of all the nos = "+ sum);
+        s.close();
+    }
+    
+}
