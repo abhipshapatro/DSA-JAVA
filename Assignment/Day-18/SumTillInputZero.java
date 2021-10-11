f7859c7c00c6f5ec563b566415a2a2f7f71febee
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2021-10-11 18:09:25 +0530

    sum of all the nos till input is 0

--- Assignment/Day-18/SumTillInputZero.java
+++ Assignment/Day-18/SumTillInputZero.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Sum {
+public class SumTillInputZero {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int sum =0;
