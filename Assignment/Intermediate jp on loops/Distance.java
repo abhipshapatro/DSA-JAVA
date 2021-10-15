0e67f373bd195950a8c5731c0808e966e4a81ce4
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2021-10-15 22:44:32 +0530

    distance between two points

---
+++ Assignment/Intermediate jp on loops/Distance.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Distance
+ */
+public class Distance {
+
+    public static void main(String[] args) {
+        System.out.println("Find the distance between 2 points");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter co-ordinates of point 1 : ");
+        int x1 = in.nextInt();
+        int y1 = in.nextInt();
+        System.out.print("Enter co-ordinates of point 2 : ");
+        int x2 = in.nextInt();
+        int y2 = in.nextInt();
+        double dist = (double) Math.sqrt((int) Math.pow(x2 - x1, 2) + (int) Math.pow(y2 - y1, 2));
+        System.out.print("Distance between 2 points = " + dist);
+        in.close();
+    }
+}
